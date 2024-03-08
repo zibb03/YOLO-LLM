e6ca998c905a73f216b2a14d354277055ffb1ffa
zibb03 <noel8044@naver.com>
2024-03-08 18:38:20 +0900

    Update yolov8.py
    Update yolov8.py to get image input & link to gradio

--- yolov8.py
+++ yolov8.py
@@ -1,22 +1,32 @@
+import cv2
 from ultralytics import YOLO
 
 class YOLO_V8:
-    img_path = 'Lenna.png'
-    class_names = []
+    def predict(self, image):
+        class_names = []
 
-    model = YOLO('yolov8n.pt')
-    results = model(img_path)
+        model = YOLO('yolov8n.pt')
+        results = model(image)
 
-    bndboxes = results[0].boxes.data
-    class_id = results[0].boxes.cls
+        bndboxes = results[0].boxes.data
+        # class_id = results[0].boxes.cls
 
-    names = results[0].names
-    # print()
-    # print(names)
+        names = results[0].names
+        # print()
+        # print(names)
 
-    # bound box
-    for i, bndbox in enumerate(bndboxes):
-        class_id = int(bndbox[5])
-        class_names.append(names[class_id])
+        # bound box
+        for i, bndbox in enumerate(bndboxes):
+            class_id = int(bndbox[5])
+            class_names.append(names[class_id])
 
-    # print(class_names)
+        return class_names
+
+if __name__ == '__main__':
+    model = YOLO_V8()
+
+    path = 'lenna.png'
+    img = cv2.imread(path)
+    result = model.predict(img)
+
+    print(result)
